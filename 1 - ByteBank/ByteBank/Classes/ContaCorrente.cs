de5874e29c1f339c436aee9b6f0f21926b3c8e09
Sandy Nascimento <sandymnascimento@outlook.com>
2022-03-04 16:48:01 -0300

    Utilizando classes.

---
+++ 1 - ByteBank/ByteBank/Classes/ContaCorrente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class ContaCorrente
+    {
+        public Cliente titular = new Cliente();
+        private double _saldo = 0;
+        private int _agencia;
+        private int _conta;
+
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+        public void Sacar(double valor)
+        {
+            if (valor > _saldo)
+            {
+                Console.WriteLine("Não foi possível realizar a operação. O valor a ser sacado é superior ao disponível.");
+                return; 
+            }
+            _saldo -= valor;
+            Console.WriteLine(titular.Nome + ", saque efetuado com sucesso! Novo saldo: " + _saldo);            
+        }
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                _saldo += valor;
+                Console.WriteLine(titular.Nome + ", o valor foi recebido com sucesso! Novo saldo: " + _saldo);
+            }                
+            else
+                Console.WriteLine("É necessário inserir um valor positivo para efetuar o depósito.");
+        }
+        public void Transferir(ContaCorrente destino, double valor)
+        {
+            if(valor <= _saldo)
+            {
+                _saldo -= valor;
+                destino.Depositar(valor);
+                return;
+            }
+            Console.WriteLine("Não foi possível realizar a operação.O valor a ser depositado é superior ao disponível.");
+        }
+    }
+}
---
+++ 1 - ByteBank/ByteBank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente cliente1 = new ContaCorrente();
+           
+            cliente1.titular.Nome = "Plinio Arruda";
+            cliente1.Agencia = 84271;
+            cliente1.Conta = 2176;
+            cliente1.titular.Cpf = 46830234496;
+
+            Console.WriteLine("Titular: " + cliente1.titular.Nome);
+            Console.WriteLine("CPF: " + cliente1.titular.Cpf);
+            Console.WriteLine("Ag. "+ cliente1.Agencia + " Cc. " + cliente1.Conta + "\n");
+                        
+            ContaCorrente cliente2 = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Marcelo Florentin";
+            cliente.Cpf = 49836234496;
+            cliente2.titular = cliente;
+            cliente2.Agencia = 84971;
+            cliente2.Conta = 2186;                       
+       
+            Console.WriteLine("Titular: " + cliente2.titular.Nome);
+            Console.WriteLine("CPF: " + cliente2.titular.Cpf);
+            Console.WriteLine("Ag. " + cliente2.Agencia + " Cc. " + cliente2.Conta + "\n");
+
+            cliente1.Depositar(1000);
+            cliente1.Transferir(cliente2, 459);
+            cliente2.Sacar(158.96);
+            Console.ReadLine();
+        }
+    }
+}
